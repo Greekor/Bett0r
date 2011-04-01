4e3cc8bd99932f5587205173e36e7b48596f24c7
Greekor <greekor@googlemail.com>
2011-04-01 13:49:41 +0200

    Some eager loading

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.includes(:odds => [{:bookie_game => :bookmaker}, :bettype]).all
     Time.zone = "CET"
   end
 end
